496e3c80c701f48a58dcc5aa25332116238aa0b2
Vladislav-Ciuperca <ciuperca37@gmail.com>
2025-03-24 19:43:08 +0100

    work done

--- JS/myscript.js
+++ JS/myscript.js
@@ -3,18 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            stato:'',
+            task:'',
             todoStuff: [
                 {
                     "task": "lavare coscienza",
-                    "state": true
+                    "state": 'done'
                 },
                 {
                     "task": "alzare le mani per goku",
-                    "state": false
+                    "state": 'not'
                 },
                 {
                     "task": "odiare gli YT rewind",
-                    "state": true
+                    "state": 'done'
                 },
                 {
                     "task": "piangere per i ninja della foglia caduti",
@@ -24,27 +26,38 @@ createApp({
         }
     },
     methods: {
-        
+
         changeState(key) {
-                        
+
             console.log(this.todoStuff);
-            
-            if(this.todoStuff[key].state === true){
-                this.todoStuff[key].state = false
-                
+
+            if (this.todoStuff[key].state === 'done') {
+                this.todoStuff[key].state = 'not'
+
             }
-            else if(this.todoStuff[key].state === false){
+            else if (this.todoStuff[key].state === 'not') {
                 this.todoStuff[key].state = 'pending'
             }
-            else{
-                this.todoStuff[key].state = true
+            else {
+                this.todoStuff[key].state = 'done'
             }
-           
+
+        },
+
+        addTask(task,stato) {
+           if(this.task && stato){
+            console.log(task,stato);
+            this.todoStuff.push({"task":task,"state":stato})
+           }else{
+            console.log("nono");
+            
+           }
+
         }
     },
-    mounted(){
+    mounted() {
 
-        
+
     }
 }).mount('#app')
 
